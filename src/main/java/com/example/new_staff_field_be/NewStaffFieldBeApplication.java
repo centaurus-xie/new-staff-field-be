9135c2041e5e68d244f67dd1a87211d0c8f546ee
xie_yuanrui <xie_yuanrui@163.com>
2025-08-11 11:21:36 +0800

    feat(all) Build Maven and Springboot frame

---
+++ src/main/java/com/example/new_staff_field_be/NewStaffFieldBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_staff_field_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewStaffFieldBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewStaffFieldBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_staff_field_be/NewStaffFieldBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_staff_field_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewStaffFieldBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
